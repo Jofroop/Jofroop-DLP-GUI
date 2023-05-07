40bbb19cc6ef872a2afc7ab597da77470753f980
Jofroop <jeffr@JDESKTOP>
2023-05-07 14:32:31 +0100

    bro it's my program i don't need to give you any information

--- Jofroop's DLP GUI/Form1.cs
+++ Jofroop's DLP GUI/Form1.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace Jofroop_s_DLP_GUI
 {
     public partial class Form1 : Form
     {
+        string dlpPath = "";
+        string outputPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "\\JofroopsDLPGUIOutput";
+
+        string strCmdLine;
+
+        OpenFileDialog dlpDialogue = new OpenFileDialog();
+        FolderBrowserDialog outputDialogue = new FolderBrowserDialog();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,66 @@ namespace Jofroop_s_DLP_GUI
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            dlpPath = Properties.Settings.Default.dlpPath;
+            outputPath = Properties.Settings.Default.outputPath;
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dlpPath == "")
+            {
+                MessageBox.Show("You must select the YT-DLP.exe file using the folder button.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                if (linkInput.Text != "")
+                {
+                    strCmdLine = "/c " + dlpPath + " " + linkInput.Text + " -o \"" + outputPath + "\\%(title)s-%(id)s.%(ext)s\"";
+                    MessageBox.Show(strCmdLine);
+                    System.Diagnostics.Process.Start("CMD.exe", strCmdLine);
+                }
+                else
+                {
+                    MessageBox.Show("The link cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void openExe_Click(object sender, EventArgs e)
+        {
+            dlpDialogue.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dlpDialogue.Filter = "Exe Files (.exe)|*.exe|All Files (*.*)|*.*";
+            dlpDialogue.ShowDialog();
+            dlpPath = dlpDialogue.FileName;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            string infoTitle = "Help";
+            string infoMessage = "Instructions for use:\n- First, download the latest version of yt-dlp or a variant of your wish.\n- Next, click the folder button and select the yt-dlp file.\n- You can now enter a link, then press the download button.\n\nFile paths:\n- By default, videos are output to C:\\Videos\\JofroopsDLGGUIOutput, but this can be changed.\n\nTroubleshooting:\n - Ensure that you've selected the yt-dlp file, and not something random.\n- Make sure that the yt-dlp file has not moved.\n - The program cannot output to folders that need admin access. Make sure you are outputting somewhere like Your Documents or Your Videos.";
+            MessageBoxButtons infoButtons = MessageBoxButtons.OK;
+
+            MessageBox.Show(infoMessage, infoTitle, infoButtons, MessageBoxIcon.Question);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.dlpPath = dlpPath;
+            Properties.Settings.Default.outputPath = outputPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void outputPathSet_Click(object sender, EventArgs e)
+        {
+            outputDialogue.ShowDialog();
+            outputPath = outputDialogue.SelectedPath;
         }
     }
 }
+
+
--- Jofroop's DLP GUI/Properties/Settings.Designer.cs
+++ Jofroop's DLP GUI/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Jofroop_s_DLP_GUI.Properties
-{
-
-
+namespace Jofroop_s_DLP_GUI.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string dlpPath {
+            get {
+                return ((string)(this["dlpPath"]));
+            }
+            set {
+                this["dlpPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string outputPath {
+            get {
+                return ((string)(this["outputPath"]));
+            }
+            set {
+                this["outputPath"] = value;
             }
         }
     }
